c6587318b84432175cd85475c58be91d3ed7ed18
esouqu <nippl3juic3@gmail.com>
2022-06-24 09:29:28 +0700

    fix: finally fix display of null in best score

--- script.js
+++ script.js
@@ -19,11 +19,8 @@ sound.forEach(s => s.volume = 0.3);
 //cp pickup volume
 sound[2].volume = 0.1;
 //set saved best score
-if (localStorage.getItem('best') === 'null') {
-    score.innerHTML = `Лучший результат: 0 рун`;
-} else {
-    score.innerHTML = `Лучший результат: ${localStorage.getItem('best')} рун`;
-}
+localStorage.setItem('best', '0')
+score.innerHTML = `Лучший результат: ${localStorage.getItem('best')} рун`;
 
 const player = {
     runes: {
